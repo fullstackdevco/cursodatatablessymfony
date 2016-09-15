fbc67c7394641636b26c2ff2f979a03a7ccdb5c5
Carlos Alfonso Perez Rivera <carlos@fullstack.com.co>
2016-09-15 10:32:59 -0500

    Instalación de DataTables Instalación de FOSRestBundle y Prueba de Api Rest.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,13 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('AppBundle:Principal:body.html.twig');
+    }
+    /**
+     * @Route("/ejemplo1", name="ejemplo1")
+     */
+    public function ejemplo1Action(Request $request)
+    {
+        return $this->render('AppBundle:Principal:datatablesestatico.html.twig');
     }
 }
